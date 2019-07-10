9a284ac0b1cbbf69c16bd5aacacf4a8356828d8a
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-07-10 20:17:38 +0900

    ajax

--- src/index.php
+++ src/index.php
@@ -7,11 +7,35 @@
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<!-- <form action="upload.php" method="post" enctype="multipart/form-data"> -->
+<form id="my_form">
     Select image to upload 
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Upload Image" name="submit" onclick="file_upload()">
 </form>
+
+<script>
+function file_upload(){
+    // フォームデータを取得
+    var formdata = new FormData($('#my_form').get(0));
+    // POSTでアップロード
+    $.ajax({
+        url  : "upload.php",
+        type : "POST",
+        data : formdata,
+        cache       : false,
+        contentType : false,
+        processData : false,
+        dataType    : "html"
+    })
+    .done(function(data, textStatus, jqXHR){
+        alert(data);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown){
+        alert("fail");
+    });
+}
+</script>
 
 </body>
 </html>
--- src/upload.php
+++ src/upload.php
@@ -1,11 +1,18 @@
 <?php
 require "function.php";
 
+$file_tmp  = $_FILES["fileToUpload"]["tmp_name"];
+$file_save = getcwd() . "/". $_FILES["fileToUpload"]["name"];
+//$result = @move_uploaded_file($file_tmp, $file_save);
+
 // POSTしたとき
-if (isset($_POST["submit"])) {
-    $getcsv = getfile($_FILES["fileToUpload"]["tmp_name"]);
-    writefile("./data.txt", $getcsv);
-    downloadfile('data.txt', 'downloadfile.txt');
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $filename = "data01.txt";
+    $download = "downloadfile.csv";
+    $getcsv = getfile($file_save);
+    writefile($filename, $getcsv);
+    //downloadfile($filename, $download);
+    echo "UPLOAD OK";
 }
 
 ?>
